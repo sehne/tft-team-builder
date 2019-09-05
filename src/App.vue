02fe9b301e0b09acaccf482c21d2640ff38e4e4f
Siggi <siegfried.sehne@gmail.com>
2019-09-05 15:27:07 +0200

    add scss, remove unused component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="TFT Team Builder"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    {{test}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      test: "Testy"
+    };
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  img {
+    background-color: red;
+  }
 }
 </style>
